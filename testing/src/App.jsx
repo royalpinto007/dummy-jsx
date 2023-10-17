19a6fe2e94c6ca1a40b2e716f046bfe509378a1c
royalpinto007 <royalpinto007@gmail.com>
2023-10-17 22:29:33 +0530

    feat: setup testing files

---
+++ testing/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Test from './components/Test'
+
+function App () {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
+      {/* <Test /> */}
+      </div>
+  )
+}
+
+export default App;
